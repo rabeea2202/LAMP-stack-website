841a7e9483ecc849d216c115b9953635fee501de
rabeea2202 <rabeeachughtai1@gmail.com>
2025-05-11 16:53:17 +0500

    Dockerfile and docker-compose.yml added

--- database.php
+++ database.php
@@ -1,12 +1,11 @@
 <?php
+$db_server = "db";
+$db_user = "root";
+$db_pass = "rootpassword";
+$db_name = "mydb";
 
-    $db_server = "localhost";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "contactdb";
-    
 
-    $conn = mysqli_connect($db_server,$db_user,$db_pass,$db_name);
-
-    
-?>
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}

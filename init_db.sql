584d4b8f574282392bbf715e1917f2f5ff85ceed
rabeea2202 <138593327+rabeea2202@users.noreply.github.com>
2025-05-18 18:16:38 +0500

    Update init_db.sql

--- init_db.sql
+++ init_db.sql
@@ -1,7 +1,9 @@
-CREATE TABLE IF NOT EXISTS sneakers (
+DROP TABLE IF EXISTS sneakers;
+
+CREATE TABLE sneakers (
   id INT AUTO_INCREMENT PRIMARY KEY,
   image VARCHAR(255),
-  name VARCHAR(100),
+  sname VARCHAR(100), 
   brand VARCHAR(100),
   price DECIMAL(10,2)
 );

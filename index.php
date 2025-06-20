dfa597fd04d37a6875aca13d5001f79c5489e9b3
rabeea2202 <138593327+rabeea2202@users.noreply.github.com>
2025-06-21 00:22:28 +0500

    Update index.php

--- index.php
+++ index.php
@@ -114,6 +114,7 @@
                     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
                 }
             } else {
+                // no image
                 echo "Failed to upload image.";
             }
         }
